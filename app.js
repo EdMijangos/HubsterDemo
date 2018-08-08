b045b17e8e39af8f64aeb869e0862b0c185a78e8
EdMijangos <mloeza2010@hotmail.com>
2018-08-08 12:46:37 -0500

    redeploying

--- app.js
+++ app.js
@@ -81,9 +81,9 @@ app.locals.title = 'Express - Generated with IronGenerator';
 const index = require('./routes/index');
 const auth = require('./routes/auth');
 const views = require('./routes/views')
-app.use('/api/', auth);
-app.use('/api/', views);  
-app.use('/api/', index);
+app.use('/', auth);
+app.use('/', views);  
+app.use('/', index);
 
 
 module.exports = app;
